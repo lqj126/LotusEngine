0221e342bf0e2686c3a8fe235334660f476e9456
unknown <1729809699@qq.com>
2023-02-09 12:58:38 +0800

    Added Buffer API

--- Lotus/src/Lotus/Application.h
+++ Lotus/src/Lotus/Application.h
@@ -10,6 +10,7 @@
 #include "Lotus/ImGui/ImGuiLayer.h"
 
 #include "Lotus/Renderer/Shader.h"
+#include "Lotus/Renderer/Buffer.h"
 
 namespace Lotus {
 
@@ -37,8 +38,10 @@ namespace Lotus {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
---
+++ Lotus/src/Lotus/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "ltpch.h"
+#include "Renderer.h"
+
+namespace Lotus {
+
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
